47a9ea6dd4f2badd6de0360f9198c19839695495
anastasiya_lebedeva <alpha.lebedeva@gmail.com>
2019-06-13 13:10:17 +0300

    clone of obj

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,19 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
-function clone (obj) {
+function clone(obj) {
+
+    var newObj = {};
+
+    for (var key in obj) {
+
+        if (typeof (obj[key]) == "object" && obj[key] != null) {
+            newObj[key] = clone(obj[key])
+        }
+        else {
+            newObj[key] = obj[key];
+        }
+
+    }
+    return newObj;
 }
